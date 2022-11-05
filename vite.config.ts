fd093eb93125258baaf3246ef27a85fea855a19e
Waylon Turbes <83845399+waylonturbes@users.noreply.github.com>
2022-11-05 14:01:21 -0700

    build: add unocss with some presets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import Unocss from "unocss/vite";
+import presetUno from "@unocss/preset-uno";
+import presetIcons from "@unocss/preset-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA(),
+    Unocss({
+      presets: [presetUno(), presetIcons()],
+    }),
+  ],
 });
